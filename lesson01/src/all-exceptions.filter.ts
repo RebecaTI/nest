a05e67f30332ce65f62e24fcfbd8648336d5a76c
RebecaTI <mebuffbeka@gmail.com>
2025-09-23 10:23:31 -0300

    feat: add throttling and custom logging functionality with MyLoggerService

---
+++ lesson01/src/all-exceptions.filter.ts
@@ -0,0 +1,24 @@
+import {
+  Catch,
+  ArgumentsHost,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Request, Response } from 'express';
+import { MyLoggerService } from './my-logger/my-logger.service';
+import { PrismaClientValidationError } from '@prisma/client/runtime/library';
+
+type MeuObjetoResponse = {
+  statusCode: number;
+  timestamp: string;
+  path: string;
+  response: string | object;
+};
+
+@Catch()
+export class AllExceptionsFilter extends BaseExceptionFilter {
+  private readonly logger = new MyLoggerService(AllExceptionsFilter.name);
+
+  catch(exception: unknown, host: ArgumentsHost) {}
+}
